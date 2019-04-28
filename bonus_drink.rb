ca9d0a69113d718374e35e0a5630aef783f2697f
You Matsumoto <youmts22@gmail.com>
2019-04-28 10:35:22 +0900

    Make total_count_for method.

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,15 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    # 最初にamount数だけ飲む
+    count = empty = amount
+
+    # 空のボトルが交換できなくなるまで続ける
+    while (additional = empty / 3) > 0
+      count += additional
+      # 空のボトルは交換できなかった分 + 今回飲んだ分
+      empty = empty % 3 + additional
+    end
+
+    count
   end
 end
